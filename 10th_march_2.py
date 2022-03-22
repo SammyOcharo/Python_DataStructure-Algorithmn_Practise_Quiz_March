d43e6ca761da0915ffc87015e5763ca6a2584e21
sammy ocharo <obanyisammy@gmail.com>
2022-03-22 23:44:30 +0300

    10th march quiz 2

---
+++ 10th_march_2.py
@@ -0,0 +1,18 @@
+##############
+aList = [4, 8, 12, 16]
+aList[1:4] = [20, 24, 28]
+print(aList)
+################
+sampleList = [10, 20, 30, 40, 50]
+sampleList.append(60)
+print(sampleList)
+
+sampleList.append(60)
+print(sampleList)
+##########################
+my_list = ["Hello", "Python"]
+print("-".join(my_list))
+########################
+aList = ["PYnative", [4, 8, 12, 16]]
+print(aList[0][1])    
+print(aList[1][3])
