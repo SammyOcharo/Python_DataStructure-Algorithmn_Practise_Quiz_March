90eb15a6f9da71f2f1791c7dbadfac6769911e0e
sammy ocharo <obanyisammy@gmail.com>
2022-03-23 10:22:55 +0300

    11th march quiz 2

---
+++ 11th_march_2.py
@@ -0,0 +1,24 @@
+"""
+Write a function called fizz_buzz that takes a number.
+If the number is divisible by 3, it should return “Fizz”.
+If it is divisible by 5, it should return “Buzz”.
+If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+Otherwise, it should return the same number.
+"""
+
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
+
+if __name__ == '__main__':
+
+    number = input("Enter the number: ")
+    number = int(number)
+
+    fizz_buzz(number)
